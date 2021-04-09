933790ec115ae405f0056db7add159077d6a815b
mkhoirulr97 <mkhoirulr97@gmail.com>
2021-04-10 00:32:34 +0700

    Create Vertex.java

---
+++ Week5/Latihan_Graph/Vertex.java
@@ -0,0 +1,15 @@
+package Latihan_Graph;
+
+/**
+ *
+ * @author khoirul
+ */
+public class Vertex {
+    public char label;
+    public boolean wasVisited;
+    
+    public Vertex (char label){
+        this.label = label;
+        wasVisited = false;
+    }
+}
