f8d34f615d1b5799ecc4b960badcd827cb7d4d69
mkhoirulr97 <mkhoirulr97@gmail.com>
2021-04-25 07:05:44 +0700

    Create A.java

---
+++ Week7/Week7_Latihan_Pewarisan/A.java
@@ -0,0 +1,15 @@
+package Week7_Latihan_Pewarisan;
+
+/**
+ *
+ * @author khoirul
+ */
+public class A {
+
+    int x;
+    int y;
+
+    void TampilkanNilaiXY() {
+        System.out.println("Nilai x : " + x + ", y : " + y);
+    }
+}
