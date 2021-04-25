74a419171182d3a2f6d21f40d184832c6cd62970
mkhoirulr97 <mkhoirulr97@gmail.com>
2021-04-25 07:04:57 +0700

    Create A.java

---
+++ Week7/Week7_Latihan_Overriding/A.java
@@ -0,0 +1,12 @@
+package Week7_Latihan_Overriding;
+
+/**
+ *
+ * @author khoirul
+ */
+public class A {
+
+    public void tampilkanKeLayar() {
+        System.out.println("Method milik class A dipanggil");
+    }
+}
