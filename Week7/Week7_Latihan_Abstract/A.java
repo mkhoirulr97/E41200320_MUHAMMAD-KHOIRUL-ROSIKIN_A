98fb24d293855f95b7e42b374f3f93366eb6a15d
mkhoirulr97 <mkhoirulr97@gmail.com>
2021-04-25 07:03:28 +0700

    Create A.java

---
+++ Week7/Week7_Latihan_Abstract/A.java
@@ -0,0 +1,14 @@
+package Week7_Latihan_Abstract;
+
+/**
+ *
+ * @author khoirul
+ */
+public abstract class A {
+
+    abstract public void method2();
+
+    public void method1() {
+        System.out.println("Mehod konkrit dari class A");
+    }
+}
