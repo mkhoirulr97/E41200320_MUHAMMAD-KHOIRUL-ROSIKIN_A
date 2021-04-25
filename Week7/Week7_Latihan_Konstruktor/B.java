cbfa48a1fa2761ef04b9143794e2ee88e87fe898
mkhoirulr97 <mkhoirulr97@gmail.com>
2021-04-25 07:04:16 +0700

    Create B.java

---
+++ Week7/Week7_Latihan_Konstruktor/B.java
@@ -0,0 +1,12 @@
+package Week7_Latihan_Konstruktor;
+
+/**
+ *
+ * @author khoirul
+ */
+public class B extends A {
+
+    B() {
+        System.out.println("Konstruktor class B dieksekusi ...");
+    }
+}
