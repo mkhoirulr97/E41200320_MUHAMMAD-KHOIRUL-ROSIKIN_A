b7a21c0624f322d858d773242945017710f9627d
mkhoirulr97 <mkhoirulr97@gmail.com>
2021-04-25 07:03:52 +0700

    Create Demo.java

---
+++ Week7/Week7_Latihan_Abstract/Demo.java
@@ -0,0 +1,14 @@
+package Week7_Latihan_Abstract;
+
+/**
+ *
+ * @author khoirul
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        B ob = new B();
+        ob.method1();
+        ob.method2();
+    }
+}
