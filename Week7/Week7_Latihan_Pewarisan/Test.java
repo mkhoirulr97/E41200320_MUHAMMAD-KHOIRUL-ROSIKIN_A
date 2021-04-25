600610199ef308494a0ed02c8ccb85a9a6298dca
mkhoirulr97 <mkhoirulr97@gmail.com>
2021-04-25 07:07:01 +0700

    Create Test.java

---
+++ Week7/Week7_Latihan_Pewarisan/Test.java
@@ -0,0 +1,14 @@
+package Week7_Latihan_Pewarisan;
+
+/**
+ *
+ * @author khoirul
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Pelajar mahasiswa = new Pelajar("Khoirul", 168, 56,
+                "E41200320", "Politeknik Negeri Jember", 98.8);
+        System.out.println(mahasiswa.toString());
+    }
+}
