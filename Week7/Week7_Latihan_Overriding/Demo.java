61e25461d94a1ad9e45c9977b6cdcc3b1e5c06a0
mkhoirulr97 <mkhoirulr97@gmail.com>
2021-04-25 07:05:34 +0700

    Create Demo.java

---
+++ Week7/Week7_Latihan_Overriding/Demo.java
@@ -0,0 +1,13 @@
+package Week7_Latihan_Overriding;
+
+/**
+ *
+ * @author khoirul
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        B subOb = new B();
+        subOb.tampilkanKeLayar();
+    }
+}
