bd9f243c1a62415f2fff8c61a2e0c609288d030b
mkhoirulr97 <mkhoirulr97@gmail.com>
2021-04-25 07:06:37 +0700

    Create Orang.java

---
+++ Week7/Week7_Latihan_Pewarisan/Orang.java
@@ -0,0 +1,22 @@
+package Week7_Latihan_Pewarisan;
+
+/**
+ *
+ * @author khoirul
+ */
+public class Orang {
+
+    private String nama;
+    private double tinggi;
+    private double berat;
+
+    public Orang(String nama, double tinggi, double berat) {
+        this.nama = nama;
+        this.tinggi = tinggi;
+        this.berat = berat;
+    }
+
+    public String toString() {
+        return ("Nama : " + nama + "\nTinggi : " + tinggi + "\nBerat : " + berat);
+    }
+}
