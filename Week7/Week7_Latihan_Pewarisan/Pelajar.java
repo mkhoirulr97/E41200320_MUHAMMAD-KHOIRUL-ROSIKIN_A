a4ba6651caa21b2cffa0a1aafef81cbd3d3ec042
mkhoirulr97 <mkhoirulr97@gmail.com>
2021-04-25 07:06:48 +0700

    Create Pelajar.java

---
+++ Week7/Week7_Latihan_Pewarisan/Pelajar.java
@@ -0,0 +1,26 @@
+package Week7_Latihan_Pewarisan;
+
+/**
+ *
+ * @author khoirul
+ */
+public class Pelajar extends Orang {
+
+    private String nim;
+    private String asalSekolah;
+    private double nilai; //range : 0-30
+
+    public Pelajar(String nama, double tinggi, double berat, String nim,
+            String sekolah, double nilai) {
+        super(nama, tinggi, berat);
+        this.nim = nim;
+        asalSekolah = sekolah;
+        this.nilai = nilai;
+    }
+
+    public String toString() {
+        return (super.toString() + "\nNIM : " + nim + "\nSekolah : " + asalSekolah
+                + "\nNilai : " + nilai);
+    }
+
+}
