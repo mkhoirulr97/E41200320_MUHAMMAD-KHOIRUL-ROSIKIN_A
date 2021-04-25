a3000dc24f8a9ef7df9abe14bf5317f2b20616c7
mkhoirulr97 <mkhoirulr97@gmail.com>
2021-04-25 07:03:39 +0700

    Create B.java

---
+++ Week7/Week7_Latihan_Abstract/B.java
@@ -0,0 +1,12 @@
+package Week7_Latihan_Abstract;
+
+/**
+ *
+ * @author khoirul
+ */
+public class B extends A {
+
+    public void method2() {
+        System.out.println("Method abstract yang mudah menjadi konkrit dari class B");
+    }
+}
